e454783facd4d037c964a1e8d698e19996183d54
matheustitton <matheustitton@gmail.com>
2024-10-09 17:58:21 -0300

    Ambiente Criado usando MariaDB

---
+++ src/back/BDReCiclo/src/test/java/ReCicloBD/BDReCiclo/BdReCicloApplicationTests.java
@@ -0,0 +1,13 @@
+package ReCicloBD.BDReCiclo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdReCicloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
